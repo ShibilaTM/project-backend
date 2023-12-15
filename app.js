82547bb75d097c8a135df2f4156211e39de39aa0
ShibilaTM <shibilaang@gmail.com>
2023-12-15 20:08:19 +0530

    first commit

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const cors = require('cors')
 require('./config/db')
 app.use(morgan('dev'))
 const PORT = process.env.PORT
-
+const path=require('path')
 app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+app.use(express.static(path.join(__dirname,'/build')));
 const userRoute = require('./routes/userRoute')
 const dashRoute = require('./routes/projectdashRoute')
 const gradeRoute = require('./routes/gradeRoute')
@@ -33,6 +33,10 @@ const subRoute = require('./routes/projectRoutes')
 app.use('/form', subRoute)
 const Studentdashboard = require('./routes/Studentdashboard')
 app.use('/studentdashboardcheck', Studentdashboard)
+
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname,'/build/index.html'));
+  })
 
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`)
